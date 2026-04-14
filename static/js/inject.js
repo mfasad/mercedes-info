ddfc34ec237756da095a97de4252e73f24f52393
GitHub Deploy Manager <deploy@github-manager.local>
2026-04-14 20:54:51 +0300

    Deploy 4 dirs, 10 files (42087 total files) via GitHub Manager

--- static/js/inject.js
+++ static/js/inject.js
@@ -1,1 +1,7 @@
-// inject.js — сюда можно добавить код счётчиков и рекламы
+// Yandex.Metrika — counter 108542226
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}
+)(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+ym(108542226,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
